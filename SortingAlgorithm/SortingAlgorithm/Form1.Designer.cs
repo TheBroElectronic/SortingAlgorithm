a81776ea486c67abed26decd1073da0d11422c2a
TheBroElectronic <69072454+TheBroElectronic@users.noreply.github.com>
2021-01-22 23:20:12 -0300

    1

---
+++ SortingAlgorithm/SortingAlgorithm/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace SortingAlgorithm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.MyAlgorithmBox = new System.Windows.Forms.CheckBox();
+            this.CalgorithmBox = new System.Windows.Forms.CheckBox();
+            this.graficBtn = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(228, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(177, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "New Order";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Image = global::SortingAlgorithm.Properties.Resources.xd;
+            this.pictureBox.Location = new System.Drawing.Point(12, 71);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(647, 450);
+            this.pictureBox.TabIndex = 1;
+            this.pictureBox.TabStop = false;
+            // 
+            // MyAlgorithmBox
+            // 
+            this.MyAlgorithmBox.AutoSize = true;
+            this.MyAlgorithmBox.Location = new System.Drawing.Point(345, 48);
+            this.MyAlgorithmBox.Name = "MyAlgorithmBox";
+            this.MyAlgorithmBox.Size = new System.Drawing.Size(106, 17);
+            this.MyAlgorithmBox.TabIndex = 4;
+            this.MyAlgorithmBox.Text = "Use my algorithm";
+            this.MyAlgorithmBox.UseVisualStyleBackColor = true;
+            this.MyAlgorithmBox.CheckedChanged += new System.EventHandler(this.MyAlgorithmBox_CheckedChanged);
+            // 
+            // CalgorithmBox
+            // 
+            this.CalgorithmBox.AutoSize = true;
+            this.CalgorithmBox.Checked = true;
+            this.CalgorithmBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.CalgorithmBox.Location = new System.Drawing.Point(187, 48);
+            this.CalgorithmBox.Name = "CalgorithmBox";
+            this.CalgorithmBox.Size = new System.Drawing.Size(106, 17);
+            this.CalgorithmBox.TabIndex = 5;
+            this.CalgorithmBox.Text = "Use c# algorithm";
+            this.CalgorithmBox.UseVisualStyleBackColor = true;
+            this.CalgorithmBox.CheckedChanged += new System.EventHandler(this.CalgorithmBox_CheckedChanged);
+            // 
+            // graficBtn
+            // 
+            this.graficBtn.AutoSize = true;
+            this.graficBtn.Location = new System.Drawing.Point(519, 14);
+            this.graficBtn.Name = "graficBtn";
+            this.graficBtn.Size = new System.Drawing.Size(110, 17);
+            this.graficBtn.TabIndex = 6;
+            this.graficBtn.Text = "Graficate Process";
+            this.graficBtn.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(671, 528);
+            this.Controls.Add(this.graficBtn);
+            this.Controls.Add(this.CalgorithmBox);
+            this.Controls.Add(this.MyAlgorithmBox);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.CheckBox MyAlgorithmBox;
+        private System.Windows.Forms.CheckBox CalgorithmBox;
+        private System.Windows.Forms.CheckBox graficBtn;
+    }
+}
+
---
+++ SortingAlgorithm/SortingAlgorithm/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SortingAlgorithm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Bitmap grafic = new Bitmap(SortingAlgorithm.Properties.Resources.xd);
+        int[] numb = new int[320];
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < 320; i++)
+            {
+                numb[i] = rnd.Next(2, 220);
+            }
+            Graficate(numb);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < 320; i++)
+            {
+                numb[i] = rnd.Next(2, 220);
+            }
+            if (MyAlgorithmBox.Checked)
+            {
+                Algorithm();
+                Graficate(numb);
+            }
+            else if (CalgorithmBox.Checked)
+            {
+                Array.Sort(numb);
+                Array.Reverse(numb);
+                Graficate(numb);
+            }
+            else Graficate(numb);
+        }
+
+        void Graficate(int[] num)
+        {
+            grafic = new Bitmap(SortingAlgorithm.Properties.Resources.xd);
+            int x2 = 0;
+            for (int x = 0; x < num.Length; x++) //leer tabla
+            {
+                int y = num[x] * 2;
+                for (int i = grafic.Height - 1; i > y; i--)//graficar tabla
+                {
+                    grafic.SetPixel(x + x2, i, Color.Black);
+                    grafic.SetPixel(x + 1 + x2, i, Color.Blue);
+                    grafic.SetPixel(x + 2 + x2, i, Color.Black);
+                }
+                x2 += 1;
+            }
+            pictureBox.Image = grafic;
+            pictureBox.Refresh();
+        }
+
+        void Algorithm()
+        {
+            int numMax = 0, posAux = 0, numAux = 0;
+            for(int pos = 0; pos < numb.Length; pos++)
+            {
+                for(int bus = pos; bus < numb.Length; bus++)
+                {
+                    if (numb[bus] > numMax)
+                    {
+                        numMax = numb[bus];
+                        posAux = bus;
+                    }
+                }
+                numAux = numb[posAux];
+                numb[posAux] = numb[pos];
+                numb[pos] = numAux;
+                numMax = 0;
+                if(graficBtn.Checked)Graficate(numb);
+            }
+        }
+
+        private void MyAlgorithmBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if(CalgorithmBox.Checked) CalgorithmBox.Checked = false;
+        }
+
+        private void CalgorithmBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if(MyAlgorithmBox.Checked) MyAlgorithmBox.Checked = false;
+        }
+    }
+}
